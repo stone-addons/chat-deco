70b2ea410c54f88a587dd25a736f07e1a07358cb
Code Hz <CodeHz@outlook.com>
2019-03-04 21:55:13 +0800

    refractor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,4 @@
-/// <reference types="stoneserver-scripting-types" />
-
-namespace Server {
-  const system = server.registerSystem<IStoneServerSystem<{}>>(0, 0);
-  system.initialize = function() {
-    system.log("Mod Loaded")
-  };
-}
+const system = server.registerSystem<IStoneServerSystem<{}>>(0, 0);
+system.initialize = function() {
+  system.log("Mod Loaded");
+};
